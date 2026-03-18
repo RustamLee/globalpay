3e5e8ccaf730e8937b7ce4eb70b87f95d067ea09
rustam sagaddinov <rustiksagad@gmail.com>
2026-03-17 23:29:24 -0300

    feat: initial project setup with docker, flyway

---
+++ src/main/java/org/example/global_pay/GlobalPayApplication.java
@@ -0,0 +1,11 @@
+package org.example.global_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalPayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalPayApplication.class, args);
+    }
+}
